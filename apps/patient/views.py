dc06496651f572b143eca5b788a4d8ac3d1fb670
jupterorbita <john.williams@outlook.com>
2018-05-24 13:43:47 -0700

    switched to patients branch added test comment in views

--- apps/patient/views.py
+++ apps/patient/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 
 
 # comment
-# a second comment
+# changed to branch patiens
 def index(request):
     response = "patient INDEX PAGE"
     return HttpResponse(response)
