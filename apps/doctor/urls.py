438969302c8486b2bb8f91b6cb5efc19520802ac
Brant Kickhofel <bkickhofel@gmail.com>
2018-05-24 13:43:46 -0700

    commit to branch doctor

--- apps/doctor/urls.py
+++ apps/doctor/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
+	url(r'login$', views.login),
+	url(r'register$', views.register),
+	url(r'register/submit$', views.regsubmit)
+	url(r'login/subit$', views.logsubmit)
 
 ]
