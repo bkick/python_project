cd952909b20a52a512576143cda1d76ec4b4680a
jupterorbita <john.williams@outlook.com>
2018-05-24 13:13:23 -0700

    added a second test comment

--- apps/patient/views.py
+++ apps/patient/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 
 
 # comment
+# a second comment
 def index(request):
     response = "patient INDEX PAGE"
     return HttpResponse(response)
